8a5de1908bbc901c934cab6b80d5f8aad253be7c
YousefNabih <yuosefnabih@gmail.com>
2026-01-05 21:07:51 +0200

    Refactor LED header to use enum for colors
    Updated LED header file to use enum for colors and changed include file.

--- OBD-II_Diagnostics/led.h
+++ OBD-II_Diagnostics/led.h
@@ -1,30 +1,33 @@
 #ifndef LEDS_H
 #define LEDS_H
 
-#include <stdint.h>
-#include "tm4c123gh6pm_registers.h"
+#include "std_types.h"
 
-#define RED_LED    0x02
-#define BLUE_LED   0x04
-#define GREEN_LED  0x08
+/* LED Colors */
+typedef enum {
+    LED_RED,
+    LED_BLUE,
+    LED_GREEN,
+    LED_ALL
+} LED_Color;
 
+/* LED Functions */
 void LED_Init(void);
-void LED_On(uint8_t color);
-void LED_Off(uint8_t color);
-void LED_Toggle(uint8_t color);
-void LED_Set(uint8_t color, uint8_t state);
-void LED_AllOff(void);
+void LED_On(uint8 color);
+void LED_Off(uint8 color);
+void LED_Toggle(uint8 color);
 
-#define RED_ON()        LED_On(RED_LED)
-#define RED_OFF()       LED_Off(RED_LED)
-#define RED_TOGGLE()    LED_Toggle(RED_LED)
+/* Simple Macros */
+#define RED_ON()      LED_On(LED_RED)
+#define RED_OFF()     LED_Off(LED_RED)
+#define RED_TOGGLE()  LED_Toggle(LED_RED)
 
-#define BLUE_ON()       LED_On(BLUE_LED)
-#define BLUE_OFF()      LED_Off(BLUE_LED)
-#define BLUE_TOGGLE()   LED_Toggle(BLUE_LED)
+#define BLUE_ON()     LED_On(LED_BLUE)
+#define BLUE_OFF()    LED_Off(LED_BLUE)
+#define BLUE_TOGGLE() LED_Toggle(LED_BLUE)
 
-#define GREEN_ON()      LED_On(GREEN_LED)
-#define GREEN_OFF()     LED_Off(GREEN_LED)
-#define GREEN_TOGGLE()  LED_Toggle(GREEN_LED)
+#define GREEN_ON()    LED_On(LED_GREEN)
+#define GREEN_OFF()   LED_Off(LED_GREEN)
+#define GREEN_TOGGLE() LED_Toggle(LED_GREEN)
 
 #endif
