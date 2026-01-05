56e0475e86f93f99080561e3ac96f1107411b69a
YousefNabih <yuosefnabih@gmail.com>
2026-01-05 21:07:22 +0200

    Refactor GPIO functions and update register access

--- OBD-II_Diagnostics/gpio.c
+++ OBD-II_Diagnostics/gpio.c
@@ -1,52 +1,66 @@
 #include "gpio.h"
+#include "tm4c123gh6pm_registers.h"
 
-static volatile uint32_t* portAddresses[] = {
-    (volatile uint32_t*)0x40004000,
-    (volatile uint32_t*)0x40005000,
-    (volatile uint32_t*)0x40006000,
-    (volatile uint32_t*)0x40007000,
-    (volatile uint32_t*)0x40024000,
-    (volatile uint32_t*)0x40025000
+/* System Control Registers */
+#define SYSCTL_RCGCGPIO_R  (*((volatile uint32 *)0x400FE608))
+#define SYSCTL_PRGPIO_R    (*((volatile uint32 *)0x400FEA08))
+
+/* GPIO Base Addresses */
+static volatile uint32* GPIO_BASE[] = {
+    (volatile uint32*)0x40004000,  /* PORTA */
+    (volatile uint32*)0x40005000,  /* PORTB */
+    (volatile uint32*)0x40006000,  /* PORTC */
+    (volatile uint32*)0x40007000,  /* PORTD */
+    (volatile uint32*)0x40024000,  /* PORTE */
+    (volatile uint32*)0x40025000   /* PORTF */
 };
 
-void GPIO_InitPort(GPIO_Port port, uint8_t pins, GPIO_Direction dir) {
+/* Register Offsets */
+#define DATA_OFFSET   0
+#define DIR_OFFSET    1
+#define DEN_OFFSET    5
+
+void GPIO_InitPin(GPIO_Port port, uint8 pin, uint8 direction) {
+    /* Enable clock for the port */
     SYSCTL_RCGCGPIO_R |= (1 << port);
+    
+    /* Wait for clock to be ready */
     while((SYSCTL_PRGPIO_R & (1 << port)) == 0);
     
-    volatile uint32_t* portBase = portAddresses[port];
+    volatile uint32* portBase = GPIO_BASE[port];
     
-    if(dir == GPIO_DIR_OUTPUT) portBase[GPIO_O_DIR] |= pins;
-    else portBase[GPIO_O_DIR] &= ~pins;
-    
-    portBase[GPIO_O_DEN] |= pins;
-}
-
-void GPIO_WritePin(GPIO_Port port, uint8_t pin, GPIO_Level level) {
-    volatile uint32_t* portBase = portAddresses[port];
-    portBase[(pin << 2)] = (level) ? pin : 0;
-}
-
-GPIO_Level GPIO_ReadPin(GPIO_Port port, uint8_t pin) {
-    volatile uint32_t* portBase = portAddresses[port];
-    return (portBase[(pin << 2)] & pin) ? GPIO_HIGH : GPIO_LOW;
-}
-
-void GPIO_TogglePin(GPIO_Port port, uint8_t pin) {
-    if(GPIO_ReadPin(port, pin) == GPIO_HIGH)
-        GPIO_WritePin(port, pin, GPIO_LOW);
-    else
-        GPIO_WritePin(port, pin, GPIO_HIGH);
-}
-
-void GPIO_SetPull(GPIO_Port port, uint8_t pin, GPIO_Pull pull) {
-    volatile uint32_t* portBase = portAddresses[port];
-    
-    if(pull == GPIO_PULL_UP)
-        portBase[GPIO_O_PUR] |= pin;
-    else if(pull == GPIO_PULL_DOWN)
-        portBase[GPIO_O_PDR] |= pin;
-    else {
-        portBase[GPIO_O_PUR] &= ~pin;
-        portBase[GPIO_O_PDR] &= ~pin;
+    /* Set direction */
+    if(direction == GPIO_OUTPUT) {
+        portBase[DIR_OFFSET] |= pin;
+    } else {
+        portBase[DIR_OFFSET] &= ~pin;
     }
+    
+    /* Enable digital function */
+    portBase[DEN_OFFSET] |= pin;
+}
+
+void GPIO_WritePin(GPIO_Port port, uint8 pin, uint8 level) {
+    volatile uint32* portBase = GPIO_BASE[port];
+    
+    if(level == GPIO_HIGH) {
+        portBase[DATA_OFFSET] |= pin;
+    } else {
+        portBase[DATA_OFFSET] &= ~pin;
+    }
+}
+
+uint8 GPIO_ReadPin(GPIO_Port port, uint8 pin) {
+    volatile uint32* portBase = GPIO_BASE[port];
+    
+    if(portBase[DATA_OFFSET] & pin) {
+        return GPIO_HIGH;
+    } else {
+        return GPIO_LOW;
+    }
+}
+
+void GPIO_TogglePin(GPIO_Port port, uint8 pin) {
+    volatile uint32* portBase = GPIO_BASE[port];
+    portBase[DATA_OFFSET] ^= pin;
 }
